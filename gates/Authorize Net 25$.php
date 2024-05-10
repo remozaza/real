45409419909f1d943238a5aba1e637e4e4a4d7cf
Lucifer2921 <131571634+Lucifer2921@users.noreply.github.com>
2024-05-10 10:19:38 +0530

    Update Authorize Net 25$.php

--- gates/Authorize Net 25$.php
+++ gates/Authorize Net 25$.php
@@ -65,7 +65,7 @@ $gate = "Authorize Net 25$";
       $userid = $update['message']['from']['id'];
 
     if (!checkAccess($userid)) {
-        $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b> @$username You're not Premium user❌</b>", $message_id);
+        $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b> @$username ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀ❌</b>", $message_id);
         exit();
     }
   $start_time = microtime(true);
@@ -78,7 +78,7 @@ $gate = "Authorize Net 25$";
 
   //====ANTISPAM AND WRONG FORMAT====//
       if (strlen($message) <= 4) {
-              sendMessage($chatId, '!𝙔𝙤𝙪 𝘿𝙪𝙢𝙗𝙤 𝘼𝙨𝙨 𝙃𝙤𝙡𝙚!%0A𝙏𝙚𝙭𝙩 𝙎𝙝𝙤𝙪𝙡𝙙 𝙊𝙣𝙡𝙮 𝘾𝙤𝙣𝙩𝙖𝙞𝙣 - <code>/atf cc|mm|yy|cvv</code>%0A𝙂𝙖𝙩𝙚𝙬𝙖𝙮 - <b>Authorize Net 25$</b>', $message_id);
+              sendMessage($chatId, 'ʏᴏᴜ ɢ@ʏ ?%0Aᴛxᴇᴛ sʜᴏᴜʟᴅ ᴄᴏɴᴛᴀɪɴ - <code>/atf cc|mm|yy|cvv</code>%0Aɢᴀᴛᴇᴡᴀʏ - <b>Authorize Net 25$</b>', $message_id);
               exit();
     }
   $r = "0";
@@ -111,7 +111,7 @@ $gate = "Authorize Net 25$";
   $last4 = substr($cc, -4);
 
 
-  $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>REVIEWING YOU'RE REQUEST ✅</b>");
+  $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>ᴅᴏɪɴɢ s*x ᴡɪᴛʜ ᴄᴄ ᴘʟs ᴡᴀɪᴛ ʙᴀʙʏ🥵...</b>");
 
   function value($str,$find_start,$find_end)
   {
